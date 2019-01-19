559f0604462fbe6c970423571c317da17b9da2a6
koiden <koiden@systena.co.jp>
2019-01-19 15:21:16 +0900

    プロジェクト立ち上げ

---
+++ src/main/java/jp/co/systena/tigerscave/blackjack/BlackJackApplication.java
@@ -0,0 +1,14 @@
+package jp.co.systena.tigerscave.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackJackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackJackApplication.class, args);
+	}
+
+}
+
